b30975887c455343584ceddebafcc05da368935b
KateVuilova <kateryna.vuilova@techmagic.co>
2018-11-14 12:49:29 +0200

    Add types

---
+++ index.d.ts
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Instance, Props } from 'tippy.js';
+
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
+
+export interface TippyProps extends Omit<Props, 'content'> {
+  content: React.ReactNode | string
+  children: React.ReactNode
+  onCreate?: (tip: Instance) => void
+}
+
+export default class Tippy extends React.Component<TippyProps> {}
