71fb761d217900cc1ebd851ad622b1d94ad792d4
atomiks <cc.glows@gmail.com>
2019-02-08 19:32:10 +1100

    Add propTypes to TippyGroup

--- src/TippyGroup.js
+++ src/TippyGroup.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import tippy from 'tippy.js'
 
 export default function TippyGroup({ children, ...props }) {
@@ -21,4 +22,8 @@ export default function TippyGroup({ children, ...props }) {
       },
     })
   })
+}
+
+TippyGroup.propTypes = {
+  children: PropTypes.arrayOf(PropTypes.element).isRequired,
 }
