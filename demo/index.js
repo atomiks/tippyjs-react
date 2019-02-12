4663e0405530b1954128eee642f55f5f8a0c3313
atomiks <cc.glows@gmail.com>
2019-02-12 16:54:12 +1100

    Import from index.js in demo

--- demo/index.js
+++ demo/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Tippy, { TippyGroup } from '../src/Tippy'
+import Tippy, { TippyGroup } from '../src'
 import './index.css'
 
 Tippy.defaultProps = {
