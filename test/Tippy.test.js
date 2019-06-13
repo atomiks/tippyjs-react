3ecfc7b9276b81cfccf5c97de4ac0507dc4bcbd2
Carbon Copy <cc.glows@gmail.com>
2019-06-14 00:20:57 +1000

    react-testing-library → @testing-library/react

--- test/Tippy.test.js
+++ test/Tippy.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Tippy from '../src/Tippy'
-import { render, cleanup } from 'react-testing-library'
+import { render, cleanup } from '@testing-library/react'
 
 afterEach(cleanup)
 
