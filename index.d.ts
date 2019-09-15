2cbf8c4ebd1352d6cf0f73758fd3b2635b4c6e3c
Carbon Copy <cc.glows@gmail.com>
2019-09-15 21:43:23 +1000

    Update types

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,11 @@
 import * as React from 'react'
-import { default as tippyCore, Instance, Options, GroupOptions } from 'tippy.js'
+import { default as tippyCore, Instance, Props } from 'tippy.js'
 
 type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
 
-export interface TippyProps extends Omit<Options, 'content'> {
+export interface TippyProps extends Omit<Props, 'content'> {
   content: React.ReactChild | React.ReactChild[]
   children: React.ReactElement<any>
-  onCreate?: (instance: Instance) => void
-  /** @deprecated Use `visible` instead */
-  isVisible?: boolean
-  /** @deprecated Use `enabled` instead */
-  isEnabled?: boolean
   visible?: boolean
   enabled?: boolean
   className?: string
@@ -19,10 +14,11 @@ export interface TippyProps extends Omit<Options, 'content'> {
 declare const Tippy: React.ForwardRefExoticComponent<TippyProps>
 export default Tippy
 
-export const tippy: typeof tippyCore;
+export const tippy: typeof tippyCore
 
-export interface TippyGroupProps extends GroupOptions {
+export interface TippySingletonProps {
   children: Array<React.ReactElement<any>>
+  delay: number | [number, number]
 }
 
-export const TippyGroup: React.FunctionComponent<TippyGroupProps>
+export const TippySingleton: React.FunctionComponent<TippySingletonProps>
