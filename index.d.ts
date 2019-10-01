5eff968acaeefa0e0c43ecd1623821c5abc61d90
atomiks <cc.glows@gmail.com>
2019-10-02 03:10:42 +1000

    Specify Partial<Props> in types (#127)
    * Specify Partial<Props>
    
    * Bump TS dep to remove Omit

--- index.d.ts
+++ index.d.ts
@@ -1,24 +1,22 @@
-import * as React from 'react'
-import { default as tippyCore, Instance, Props, Plugin } from 'tippy.js'
+import * as React from 'react';
+import {default as tippyCore, Instance, Props, Plugin} from 'tippy.js';
 
-type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
-
-export interface TippyProps extends Omit<Props, 'content'> {
-  content: React.ReactChild | React.ReactChild[]
-  children: React.ReactElement<any>
-  visible?: boolean
-  enabled?: boolean
-  className?: string
-  plugins?: Plugin[]
+export interface TippyProps extends Omit<Partial<Props>, 'content'> {
+  content: React.ReactChild | React.ReactChild[];
+  children: React.ReactElement<any>;
+  visible?: boolean;
+  enabled?: boolean;
+  className?: string;
+  plugins?: Plugin[];
 }
 
-declare const Tippy: React.ForwardRefExoticComponent<TippyProps>
-export default Tippy
+declare const Tippy: React.ForwardRefExoticComponent<TippyProps>;
+export default Tippy;
 
-export const tippy: typeof tippyCore
+export const tippy: typeof tippyCore;
 
-export interface TippySingletonProps extends Props {
-  children: Array<React.ReactElement<any>>
+export interface TippySingletonProps extends Partial<Props> {
+  children: Array<React.ReactElement<any>>;
 }
 
-export const TippySingleton: React.FunctionComponent<TippySingletonProps>
+export const TippySingleton: React.FunctionComponent<TippySingletonProps>;
