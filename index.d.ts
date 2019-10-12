7eb1c8ec9d5a0e01ecec3b0eba0613d833a4dd99
atomiks <cc.glows@gmail.com>
2019-10-12 12:06:09 +1100

    Use KnownProps to ignore index signature (#137)
    * Fix: Use KnownProps to ignore index signature
    
    * Add index signature to extended types
    
    Allows backwards-compatibility for typeless plugin props
    
    * Use KnownSingletonProps for useSingleton() types

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,15 @@
 import * as React from 'react';
 import {default as tippyCore, Instance, Props, Plugin} from 'tippy.js';
 
-export interface TippyProps extends Omit<Partial<Props>, 'content'> {
+type KnownKeys<T> = {
+  [K in keyof T]: string extends K ? never : number extends K ? never : K;
+} extends {[_ in keyof T]: infer U}
+  ? U
+  : never;
+
+export type KnownProps = Pick<Props, KnownKeys<Props>>;
+
+export interface TippyProps extends Omit<Partial<KnownProps>, 'content'> {
   content: React.ReactChild | React.ReactChild[];
   children: React.ReactElement<any>;
   visible?: boolean;
@@ -9,6 +17,7 @@ export interface TippyProps extends Omit<Partial<Props>, 'content'> {
   className?: string;
   plugins?: Plugin[];
   singleton?: (instance: Instance) => void;
+  [key: string]: any;
 }
 
 declare const Tippy: React.ForwardRefExoticComponent<TippyProps>;
@@ -16,15 +25,24 @@ export default Tippy;
 
 export const tippy: typeof tippyCore;
 
-export interface TippySingletonProps extends Partial<Props> {
+export interface TippySingletonProps extends Partial<KnownProps> {
   children: Array<React.ReactElement<any>>;
   className?: string;
   plugins?: Plugin[];
+  [key: string]: any;
 }
 
 export const TippySingleton: React.FunctionComponent<TippySingletonProps>;
 
-export type UseSingletonProps = Omit<TippySingletonProps, 'children'>;
+type KnownSingletonProps = Pick<
+  TippySingletonProps,
+  KnownKeys<TippySingletonProps>
+>;
+
+export interface UseSingletonProps
+  extends Omit<KnownSingletonProps, 'children'> {
+  [key: string]: any;
+}
 
 export const useSingleton: (
   props?: UseSingletonProps,
