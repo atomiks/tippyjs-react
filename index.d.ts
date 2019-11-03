3c5d555a5e0b07a1e24144c98a5c43ff9e5f1b8e
Carbon Copy <cc.glows@gmail.com>
2019-11-04 01:30:18 +1100

    fix(types): strict types for plugins
    Since tippy.js@5.1.1

--- index.d.ts
+++ index.d.ts
@@ -1,22 +1,13 @@
 import * as React from 'react';
 import {default as tippyCore, Instance, Props, Plugin} from 'tippy.js';
 
-type KnownKeys<T> = {
-  [K in keyof T]: string extends K ? never : number extends K ? never : K;
-} extends {[_ in keyof T]: infer U}
-  ? U
-  : never;
-
-export type KnownProps = Pick<Props, KnownKeys<Props>>;
-
-export interface TippyProps extends Omit<Partial<KnownProps>, 'content'> {
+export interface TippyProps extends Omit<Partial<Props>, 'content'> {
   content: React.ReactChild | React.ReactChild[];
   children: React.ReactElement<any>;
   visible?: boolean;
   enabled?: boolean;
   className?: string;
   singleton?: (instance: Instance) => void;
-  [key: string]: any;
 }
 
 declare const Tippy: React.ForwardRefExoticComponent<TippyProps>;
@@ -24,24 +15,15 @@ export default Tippy;
 
 export const tippy: typeof tippyCore;
 
-export interface TippySingletonProps extends Partial<KnownProps> {
+export interface TippySingletonProps extends Partial<Props> {
   children: Array<React.ReactElement<any>>;
   enabled?: boolean;
   className?: string;
-  [key: string]: any;
 }
 
 export const TippySingleton: React.FunctionComponent<TippySingletonProps>;
 
-type KnownSingletonProps = Pick<
-  TippySingletonProps,
-  KnownKeys<TippySingletonProps>
->;
-
-export interface UseSingletonProps
-  extends Omit<KnownSingletonProps, 'children'> {
-  [key: string]: any;
-}
+export type UseSingletonProps = Omit<TippySingletonProps, 'children'>;
 
 export const useSingleton: (
   props?: UseSingletonProps,
